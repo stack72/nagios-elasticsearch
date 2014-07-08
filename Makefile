2cb847192a0c2c13494b652cc8912818736ba454
stack72 <public@paulstack.co.uk>
2014-07-08 13:26:02 +0100

    Adding a .travis file and also a Makefile to allow us to test it

---
+++ Makefile
@@ -0,0 +1,5 @@
+install:
+	python setup.py install
+
+develop:
+	python setup.py develop
--- setup.py
+++ setup.py
@@ -3,13 +3,21 @@ from pip.req import parse_requirements
 
 setup(
     name="nagios-elasticsearch",
-    description="A selection of Nagios plugins to monitor ElasticSearch",
+    description="A selection of Nagios plugins to monitor ElasticSearch.",
+    long_description="""
+nagios-elasticsearch is a selection of nagios plugins that will monitor
+a selection of ElasticSearch cluster metrics like Unassigned Shards, Cluster
+health, that all nodes in the cluster are accounted for and also the Cluster
+JVM settings 
+    """,
     version="0.1.0",
     packages=find_packages(),
-    url="https://github.com/anchor/nagios-plugin-elasticsearch",
+    author='Paul Stack'
+    author_email='pstack@opentable.com'
+    url="https://github.com/opentable/nagios-elasticsearch",
     scripts=["check_es_nodes.py"],
     license="MIT",
     install_requires=[str(req.req) for req in
-                          parse_requirements("requirements.txt")],
+                      parse_requirements("requirements.txt")],
     include_package_data=True
 )
