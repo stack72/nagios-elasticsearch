ae5cf653f56a26bd3381ca711a085da3706e00ef
stack72 <public@paulstack.co.uk>
2014-07-08 13:49:12 +0100

    Updating the travis details so that we can actually get a build working

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ setup(
 nagios-elasticsearch is a selection of nagios plugins that will monitor
 a selection of ElasticSearch cluster metrics like Unassigned Shards, Cluster
 health, that all nodes in the cluster are accounted for and also the Cluster
-JVM settings 
+JVM settings
     """,
     version="0.1.0",
     packages=find_packages(),
-    author='Paul Stack'
-    author_email='pstack@opentable.com'
+    author='Paul Stack',
+    author_email='pstack@opentable.com',
     url="https://github.com/opentable/nagios-elasticsearch",
     scripts=["check_es_nodes.py"],
     license="MIT",
