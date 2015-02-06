b2be00d938450df309dd9d760bc09a5e866eed52
stack72 <public@paulstack.co.uk>
2015-02-06 11:21:36 +0000

    Fixing the package up for use on pypi

--- setup.py
+++ setup.py
@@ -10,14 +10,18 @@ a selection of ElasticSearch cluster metrics like Unassigned Shards, Cluster
 health, that all nodes in the cluster are accounted for and also the Cluster
 JVM settings
     """,
-    version="0.1.0",
+    version="0.1",
     packages=find_packages(),
     author='Paul Stack',
-    author_email='pstack@opentable.com',
+    author_email='public@paulstack.co.uk',
     url="https://github.com/opentable/nagios-elasticsearch",
-    scripts=["check_es_nodes.py"],
+    download_url = 'https://github.com/stack72/nagios-elasticsearch/tarball/0.1',
+    scripts=["check_es_nodes.py","check_es_cluster_status.py","check_es_jvm_usage.py", "check_es_unassigned_shards.py","check_es_split_brain.py"],
     license="MIT",
     install_requires=[str(req.req) for req in
                       parse_requirements("requirements.txt")],
-    include_package_data=True
+    include_package_data=True,
+    keywords = ['monitoring','nagios','elasticsearch'],
+    classifiers=[],
+
 )
